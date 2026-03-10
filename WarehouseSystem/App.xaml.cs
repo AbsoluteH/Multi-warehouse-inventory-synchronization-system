7cdab949e09a52a26d500777f2f395708de5fda7
wyn <wyn1238888@163.com>
2026-03-10 13:56:19 +0800

    向主界面添加容器和依赖注入

--- WarehouseSystem/App.xaml.cs
+++ WarehouseSystem/App.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using WarehouseSystem.Views;
+using WarehouseSystem.ViewModels;
 
 namespace WarehouseSystem
 {
@@ -9,6 +13,32 @@ namespace WarehouseSystem
     /// </summary>
     public partial class App : Application
     {
+        private readonly IHost _host;
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddSingleton<MainWindowView>();
+                    services.AddSingleton<MainWindowViewModel>();
+                    // 注册其他服务...
+                })
+                .Build();
+        }
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await _host.StartAsync();
+            var mainWindow = _host.Services.GetRequiredService<MainWindowView>();
+            mainWindow.DataContext = _host.Services.GetRequiredService<MainWindowViewModel>();
+            mainWindow.Show();
+            base.OnStartup(e);
+        }
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await _host.StopAsync();
+            _host.Dispose();
+            base.OnExit(e);
+        }
     }
 
 }
